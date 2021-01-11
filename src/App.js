1a093d8a84673f7eb70473bee846772ad19781eb
Linda Sundvall <73383816+L-Sundvall@users.noreply.github.com>
2021-01-11 13:32:12 +0100

    added content in APP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-     
+  <h1> Hello World</h1>
         <p>
           <Weather />
         </p>
--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
 import axios from "axios";
 
 export default function Weather (){
     function showWeather(response){
     alert (`The temp in New york is ${response.data.main.temp}`);
 }
-
     let apiKey = "7e499109a815c2c14463aa26aad21ebb";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(showWeather);}
+    axios.get(apiUrl).then(showWeather);
+    
+}
